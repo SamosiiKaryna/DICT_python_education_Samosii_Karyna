7f1d8fd57b9177be9ce26fd2be0ede904e5e3b2e
SamosiiKaryna <k.a.samosii@student.khai.edu>
2022-10-23 20:42:34 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,3 +1,6 @@
 print("Hello! My name is DICT_Bot.")
 print("I was created in 2022")
 
+print("Please, remind me your name")
+user_input = input(">")
+print("What a great name you have,",user_input,"!")
